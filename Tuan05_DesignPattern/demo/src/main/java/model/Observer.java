51b5e1f255ebf1ffdf66b3f313bfc0b17e4c42de
HuyenTranz <tranthihuyentran982003@gmail.com>
2025-03-22 21:25:22 +0700

    Tuan05 Kien truc va thiet ke phan mem

--- Tuan05_DesignPattern/demo/src/main/java/model/Observer.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public interface Observer {
-    // Phương thức update() được gọi khi Subject có thay đổi và cần thông báo cho Observer.
-    public void update();
-
-    // Phương thức setSubject() dùng để liên kết Observer với một đối tượng Subject cụ thể.
-    public void setSubject(Subject sub);
-}
